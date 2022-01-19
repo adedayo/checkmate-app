c4424068e2e01779d2593f9689bd3c3202c2ac77
Adedayo Adetoye <3177640+adedayo@users.noreply.github.com>
2022-01-19 21:07:20 +0000

    Fix windows build

---
+++ download-checkmate.ps1
@@ -0,0 +1,15 @@
+$Response = Invoke-WebRequest -UseBasicParsing -Uri https://api.github.com/repos/adedayo/checkmate/releases/latest
+
+$artefacts = $Response.Content | jq -r ".assets[].browser_download_url"
+
+$artefacts | ForEach-Object -Process {
+  if ($_ -like '*windows*') {
+     Remove-Item "checkmate-binary" -Filter * -Recurse -ErrorAction Ignore
+     New-Item -ItemType Directory -Path "checkmate-binary\windows"
+     Invoke-WebRequest -Uri $_ -OutFile "checkmate.tar.gz"
+     tar -xvzf ".\checkmate.tar.gz"
+     Move-Item -Path .\checkmate.exe -Destination .\checkmate-binary\windows
+  }
+
+}
+
