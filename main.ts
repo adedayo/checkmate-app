f980986c46afd1580cdd361a18e8249de8b24d29
Adedayo Adetoye <3177640+adedayo@users.noreply.github.com>
2022-02-01 21:24:32 +0000

    Before upgrade to electron 17

--- main.ts
+++ main.ts
@@ -125,7 +125,7 @@ function createWindow(): BrowserWindow {
       devTools: (serve) ? true : false,
       allowRunningInsecureContent: false,
       contextIsolation: false,  // false if you want to run 2e2 test with Spectron
-      enableRemoteModule: true // true if you want to run 2e2 test  with Spectron or use remote module in renderer context (ie. Angular)
+      // enableRemoteModule: true // true if you want to run 2e2 test  with Spectron or use remote module in renderer context (ie. Angular)
     },
   });
 
